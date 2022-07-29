08d288b61ab0973dbf1835971559fff381dfe4c2
Tobias Legenden <tobiaslegenden@gmail.com>
2022-07-30 01:35:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# python csv_writer
+Author: Tobias Legenden \
+Email: tobiaslegenden@gmail.com
+# Thanks to
+Reval Govender @ https://www.youtube.com/c/RevalGovender \
+Tutorial: https://www.youtube.com/watch?v=iy4N5Y3bRvA
---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+
+gender_is_female_and_like_rock_music = []
+
+# Read csv
+with open('dataset.csv', 'r') as dataset_csv:
+    csv_dict_reader = csv.DictReader(dataset_csv)
+
+    for row in csv_dict_reader:
+        # Gender must be female.
+        # Female favorite music must be Rock.
+        gender = row['Gender']
+        music = row['Favorite Music Genre']
+        if gender == 'F' and music == 'Rock':
+            # Parse csv
+            gender_is_female_and_like_rock_music.append({
+                'Gender': row['Gender'],
+                'Favorite Music Genre': row['Favorite Music Genre']
+            })
+
+# Write new csv file
+with open(
+        'gender_is_female_and_like_rock_music.csv',
+        'w',
+        newline=''
+) as gender_is_female_and_like_rock_music_csv:
+    headers = ['Favorite Music Genre', 'Gender']
+    csv_dict_writer = csv.DictWriter(gender_is_female_and_like_rock_music_csv, fieldnames=headers)
+    csv_dict_writer.writeheader()
+
+    for gender in gender_is_female_and_like_rock_music:
+        csv_dict_writer.writerow(gender)
